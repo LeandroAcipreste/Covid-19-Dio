bd9a91d8a2cb5243a2b803787b449e88f5f6e752
LeandroAcipreste <logacipreste@gmail.com>
2022-05-18 10:36:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StylesProvider } from '../node_modules/@material-ui/core/index';
+import { CssBaseline } from '../node_modules/@material-ui/core/index';
+import GlobalStyle from './commons/constants/utils/styles/Global-style';
+import Main from './contaniners/Main/index'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StylesProvider injectFirst>
+      <CssBaseline/>
+      <GlobalStyle/>
+      <Main/>
+    </StylesProvider>
+
   );
 }
 
---
+++ src/contaniners/components/Board.jsx
@@ -0,0 +1,38 @@
+import React, {memo} from 'react'
+import propTypes from 'prop-types'
+import {Grid, Skeleton} from '../../components'
+import { getByDisplayValue } from '@testing-library/react'
+import Card from './Card'
+
+function Board({ data }) {
+  const { cases, todayDeaths, recovered, deaths, todayCases } = data
+
+  const getValue = (value) => value ? value : <Skeleton variant="text" width={182} height={60} />
+
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(cases)} label="Total de casos" color="#5d78ff"/>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(todayDeaths)} label="Óbitos de hoje" color="#f7b829"/>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(todayCases)} label="Casos de hoje" color="#000"/>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(deaths)} label="Total de mortes" color="#E95078"/>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(recovered)} label="Total de recuperados" color="#67C887"/>
+            </Grid>
+            
+        </Grid>
+
+    )
+}
+
+export default Board
+
+
+
